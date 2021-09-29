363ff84e4cd55cd7c1f4937a0dc0df6551038f0d
Chuck Liu <chuckliu@Chucks-MacBook-Pro.local>
2021-09-28 23:14:49 -0700

    Create the init framework

---
+++ EgroceryWebParent/EgroceryBackEnd/src/main/java/com/shopme/egrocerybackend/EgroceryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.egrocerybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgroceryBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgroceryBackEndApplication.class, args);
+    }
+
+}
---
+++ EgroceryWebParent/EgroceryFrontEnd/src/main/java/com/shopme/egroceryfrontend/EgroceryFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.egroceryfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgroceryFrontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgroceryFrontEndApplication.class, args);
+    }
+
+}
